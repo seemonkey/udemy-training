48d50c92f74d801740ab269f360707a52233bed4
Sue Virgilio <slvirgilio@gmail.com>
2016-10-02 19:22:05 -0400

    this is not working

---
+++ 02_package/main/main.go
@@ -0,0 +1,9 @@
+package main
+import (
+  "fmt"
+  "$GOPATH/src/github.com/seemonkey/udemy_training/02_package/stringutil"
+)
+func main() {
+  fmt.Println(stringutil.Reverse("!oG ,olleH"))
+  fmt.Println(stringutil.MyName)
+}
